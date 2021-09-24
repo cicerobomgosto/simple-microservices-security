52b7b3502aa450fc3ca111c427331fb953436c0d
cicerobomgosto <73124154+cicerobomgosto@users.noreply.github.com>
2021-09-24 20:09:37 -0300

    Add files via upload

---
+++ config-server/src/main/java/br/com/brdev2w/configserver/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package br.com.brdev2w.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
